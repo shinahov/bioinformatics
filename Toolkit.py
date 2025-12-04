997ff07bc8cddf4ff47e678b47b143ff4c75678c
shinahov <zzibra07@gmail.com>
2025-12-04 13:48:19 +0100

    added translate_rna func

--- Toolkit.py
+++ Toolkit.py
@@ -3,6 +3,38 @@ import collections
 valid_nucleotides = {'A', 'C', 'G', 'T'}
 pairs = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
 
+codon_table = {
+    "UUU": "F", "UUC": "F",
+    "UUA": "L", "UUG": "L",
+    "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S",
+    "UAU": "Y", "UAC": "Y",
+    "UAA": "Stop", "UAG": "Stop",
+    "UGU": "C", "UGC": "C",
+    "UGA": "Stop",
+    "UGG": "W",
+
+    "CUU": "L", "CUC": "L", "CUA": "L", "CUG": "L",
+    "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+    "CAU": "H", "CAC": "H",
+    "CAA": "Q", "CAG": "Q",
+    "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R",
+
+    "AUU": "I", "AUC": "I", "AUA": "I",
+    "AUG": "M",
+    "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+    "AAU": "N", "AAC": "N",
+    "AAA": "K", "AAG": "K",
+    "AGU": "S", "AGC": "S",
+    "AGA": "R", "AGG": "R",
+
+    "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V",
+    "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+    "GAU": "D", "GAC": "D",
+    "GAA": "E", "GAG": "E",
+    "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G"
+}
+
+
 def validate_sequence(sequence):
     sequence = sequence.upper()
     for nucleotide in sequence:
@@ -42,3 +74,15 @@ def prob_of_dominant_phenotype(p3, p2, p1):
     AaxAa = (p2 / total_offspring) * (p2 - 1) / (total_offspring - 1)
     prob = 1 - (aaxaa + 0.5 * Aaxaa + 0.25 * AaxAa)
     return prob
+
+def translate_rna(rna_sequence):
+    protein_sequence = []
+    for i in range(0, len(rna_sequence), 3):
+        codon = rna_sequence[i:i+3]
+        acid = codon_table.get(codon)
+        if acid == "Stop" or acid is None:
+            continue
+        protein_sequence.append(acid)
+    return ''.join(protein_sequence)
+
+
